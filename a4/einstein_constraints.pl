3431fe0ad4f314b7176cd7dc3e507e35fb71e688
Till Theis <theis.till@gmail.com>
2013-06-15 14:56:54 +0200

    working with 37k inferences

--- a4/einstein_constraints.pl
+++ a4/einstein_constraints.pl
@@ -1,34 +1,16 @@
 :- use_module(library(clpfd)).
 
 
-%% colors :- [red, green, ivory, blue, yellow].
-%% nationalities :- [englishman, spaniard, ukrainian, norwegian, japanese].
-%% pets :- [dog, snake, zebra, fox, horse].
-%% drinks :- [tea, orange_juice, milk, water, coffee].
-%% cigarettes :- [old_gold, kools, chesterfield, lucky_strike, parliament].
+my_nth1(X, Xs, Idx) :- nth1(Idx, Xs, X).
 
-%% house([Color, Nationality, Pet, Drink, Cigarette]) :-
-%%   member(Color, colors),
-%%   member(Nationality, nationalities),
-%%   member(Pet, pets),
-%%   member(Drink, drinks),
-%%   member(Cigarette, cigarettes).
+atomified_house(NumberedHouse, House) :-
+  Colors = [red, green, ivory, blue, yellow],
+  Nationalities = [englishman, spaniard, ukrainian, norwegian, japanese],
+  Pets = [dog, snake, zebra, fox, horse],
+  Drinks = [tea, orange_juice, milk, water, coffee],
+  Cigarettes = [old_gold, kools, chesterfield, lucky_strike, parliament],
 
-
-
-
-house([Color, Nationality, Pet, Drink, Cigarette]) :-
-  color(Color),
-  nationality(Nationality),
-  pet(Pet),
-  drink(Drink),
-  cigarette(Cigarette).
-
-right_of(Right, Left, [Left, Right | _]).
-right_of(Right, Left, [_ | Houses]) :- right_of(Right, Left, Houses).
-
-next_to(One, Two, Houses) :- right_of(One, Two, Houses).
-next_to(One, Two, Houses) :- right_of(Two, One, Houses).
+  maplist(nth1, NumberedHouse, [Colors, Nationalities, Pets, Drinks, Cigarettes], House).
 
 
 solve(Houses) :-
@@ -43,12 +25,6 @@ solve(Houses) :-
   Pets ins 1..5,
   Drinks ins 1..5,
   Cigarettes ins 1..5,
-
-  all_distinct(Colors),
-  all_distinct(Nationalities),
-  all_distinct(Pets),
-  all_distinct(Drinks),
-  all_distinct(Cigarettes),
 
   Englishman #= Red,
   Spaniard #= Dog,
@@ -65,11 +41,25 @@ solve(Houses) :-
   Japanese #= Parliament,
   (Norwegian #= (Blue + 1)) #\/ (Norwegian #= (Blue - 1)),
 
-  transpose([Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouses),
-  maplist(label, NumberedHouses), % required to resolve vars
+  all_distinct(Colors),
+  all_distinct(Nationalities),
+  all_distinct(Pets),
+  all_distinct(Drinks),
+  all_distinct(Cigarettes),
 
-  writeln([Colors, Nationalities, Pets, Drinks, Cigarettes]),
-  writeln(NumberedHouses).
+  % uncomment this for testing to find out the number of inferences
+  %% Houses = [Colors, Nationalities, Pets, Drinks, Cigarettes],
+  %% maplist(label, Houses).
+
+  % how could this be shortened?
+  maplist(my_nth1(1), [Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouse1),
+  maplist(my_nth1(2), [Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouse2),
+  maplist(my_nth1(3), [Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouse3),
+  maplist(my_nth1(4), [Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouse4),
+  maplist(my_nth1(5), [Colors, Nationalities, Pets, Drinks, Cigarettes], NumberedHouse5),
+
+  NumberedHouses = [NumberedHouse1, NumberedHouse2, NumberedHouse3, NumberedHouse4, NumberedHouse5],
+  maplist(atomified_house, NumberedHouses, Houses).
 
 print_solution(Hs) :- print_solution(1, Hs).
 print_solution(_, []).
@@ -78,3 +68,5 @@ print_solution(N, [H | Hs]) :-
   format("House ~w: ~w ~w ~w ~w ~w\n", [N, Color, Nationality, Pet, Drink, Cigarette]),
   N_ is N + 1,
   print_solution(N_, Hs).
+
+solve_and_print :- solve(Hs), print_solution(Hs).
